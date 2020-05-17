0d29dab3828bfc17287f869eb7aa2d8e15a36d7e
Dhosio <danielgithiomi@gmail.com>
2020-05-17 20:53:33 +0300

    Added button on-click listener

--- app/src/main/java/com/moringaschool/myrestaurant/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/MainActivity.java
@@ -3,12 +3,24 @@ package com.moringaschool.myrestaurant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+            mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //
+                }
+            });
     }
 }
