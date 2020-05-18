f489b14f55368492ef3ac801f93a88c23f2fc6e7
Dhosio <danielgithiomi@gmail.com>
2020-05-18 22:52:35 +0300

    Implemented the array adapter

--- app/src/main/java/com/moringaschool/myrestaurant/RestaurantsActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/RestaurantsActivity.java
@@ -4,12 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class RestaurantsActivity extends AppCompatActivity {
 
     public static final String TAG = RestaurantsActivity.class.getSimpleName();
     private TextView mNameView;
+    private ListView mRestaurants;
+
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +32,23 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
 
         mNameView = (TextView)findViewById(R.id.nameTextView);
+        mRestaurants = (ListView)findViewById(R.id.lvRestaurants);
 
-        Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
-        mNameView.setText("Confirm that your name is: " + name);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        mRestaurants.setAdapter(adapter);
+
+            mRestaurants.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    String restaurant = ((TextView)view).getText().toString();
+                    Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+                }
+            });
+
+        final Intent intent = getIntent();
+        final String name = intent.getStringExtra("name");
+        mNameView.setText(name + ", these are closest you!");
+
+
     }
 }
