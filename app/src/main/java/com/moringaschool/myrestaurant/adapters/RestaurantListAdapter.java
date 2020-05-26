c46c5aa11f7a2da4e8a4b55c8218cdd78f9ddba5
Dhosio <danielgithiomi@gmail.com>
2020-05-26 13:56:10 +0300

    Implemented methods for the custom recycler view adapter

--- app/src/main/java/com/moringaschool/myrestaurant/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/moringaschool/myrestaurant/adapters/RestaurantListAdapter.java
@@ -1,6 +1,38 @@
 package com.moringaschool.myrestaurant.adapters;
 
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestauantViewHolder> {
+import com.moringaschool.myrestaurant.Business;
+
+import java.util.List;
+
+public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAdapter.RestaurantViewHolder> {
+
+    public List<Business> mBusinesses;
+    public Context mContext;
+
+    public RestaurantListAdapter(Context context, List<Business> businesses){
+        this.mBusinesses = businesses;
+        this.mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public RestaurantListAdapter.RestaurantViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RestaurantListAdapter.RestaurantViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mBusinesses.size();
+    }
 }
