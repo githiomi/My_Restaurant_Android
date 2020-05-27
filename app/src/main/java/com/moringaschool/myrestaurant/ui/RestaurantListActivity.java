269ccd42cdfeae92d118b1f6c28006889ab1e3df
Dhosio <danielgithiomi@gmail.com>
2020-05-27 18:55:58 +0300

    Imported the parceler library and added it to the business class

--- app/src/main/java/com/moringaschool/myrestaurant/ui/RestaurantListActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/ui/RestaurantListActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.moringaschool.myrestaurant.R;
 import com.moringaschool.myrestaurant.adapters.MyRestaurantsArrayAdapter;
@@ -162,6 +164,13 @@ public class RestaurantListActivity extends AppCompatActivity {
                             ArrayAdapter<String> adapter = new MyRestaurantsArrayAdapter(RestaurantListActivity.this,
                                     android.R.layout.simple_list_item_1, restaurantNames, categories, images );
                             mRestaurantsListView.setAdapter(adapter);
+
+                            mRestaurantsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                    Toast.makeText(RestaurantListActivity.this, restaurantNames[position], Toast.LENGTH_SHORT).show();
+                                }
+                            });
 
                             for (Restaurant restaurant : mRestaurants) {
                                 Log.d(TAG, "Name: " + restaurant.getName());
