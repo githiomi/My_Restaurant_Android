9bb412582b459542f6ff626533bb35c9bc46f78a
Dhosio <danielgithiomi@gmail.com>
2020-05-28 12:53:56 +0300

    Correcting the Restaurant list activity

--- app/src/main/java/com/moringaschool/myrestaurant/ui/RestaurantListActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/ui/RestaurantListActivity.java
@@ -1,7 +1,6 @@
 package com.moringaschool.myrestaurant.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,12 +8,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.moringaschool.myrestaurant.R;
 import com.moringaschool.myrestaurant.YelpBusinessesSearchResponse;
@@ -48,8 +45,8 @@ public class RestaurantListActivity extends AppCompatActivity {
     @BindView(R.id.errorTextView)
     TextView mErrorTextView;
 
-    public List<Business> mRestaurants = new ArrayList<>();
-//    public List<Restaurant> mRestaurants = new ArrayList<>();
+//        public List<Business> mRestaurants = new ArrayList<>();
+    public List<Restaurant> mRestaurants = new ArrayList<>();
 
 
     @Override
@@ -77,9 +74,9 @@ public class RestaurantListActivity extends AppCompatActivity {
             public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
 
                 if (response.isSuccessful()) {
-                    mRestaurants = response.body().getBusinesses();
+                    mRestaurants = (Restaurant) response.body().getBusinesses();
 
-                    for (Business restaurant : mRestaurants){
+                    for (Restaurant restaurant : mRestaurants){
                         Log.v(TAG, restaurant.getName());
                     }
 
@@ -129,70 +126,63 @@ public class RestaurantListActivity extends AppCompatActivity {
         mProgressBar.setVisibility(View.GONE);
     }
 
-//        private void getRestaurants (String location) {
-//            final YelpService yelpService = new YelpService();
-//            yelpService.findRestaurants(location, new Callback() {
+//    private void getRestaurants (String location) {
+//        final YelpService yelpService = new YelpService();
+//        yelpService.findRestaurants(location, new Callback() {
 //
-//                @Override
-//                public void onFailure(Call call, IOException e) {
-//                    e.printStackTrace();
-//                }
+//            @Override
+//            public void onFailure(Call call, IOException e) {
+//                e.printStackTrace();
+//            }
 //
-//                @Override
-//                public void onResponse(Call call, Response response) {
+//            @Override
+//            public void onResponse(Call call, Response response) {
 //
-//                    mRestaurants = yelpService.processResults(response);
+//                mRestaurants = yelpService.processResults(response);
 //
-//                    RestaurantListActivity.this.runOnUiThread(new Runnable() {
+//                RestaurantListActivity.this.runOnUiThread(new Runnable() {
 //
-//                        @Override
-//                        public void run() {
+//                    @Override
+//                    public void run() {
 //
-//                            hideProgressBar();
-//                            showRestaurants();
+//                        hideProgressBar();
+//                        showRestaurants();
 //
-//                            String[] restaurantNames = new String[mRestaurants.size()];
-//                            String[] categories = new String[mRestaurants.size()];
-//                            String[] images = new String[mRestaurants.size()];
+//                        String[] restaurantNames = new String[mRestaurants.size()];
+//                        String[] categories = new String[mRestaurants.size()];
+//                        String[] images = new String[mRestaurants.size()];
 //
-//                            for (int i = 0; i < restaurantNames.length; i++) {
-//                                restaurantNames[i] = mRestaurants.get(i).getName();
-//                            }
-//
-//                            for (int z = 0; z < categories.length; z += 1){
-//                                String category = mRestaurants.get(z).getCategories().get(0);
-//                                categories[z] = category;
-//                            }
-//
-//                            for (int a = 0; a < images.length; a += 1 ){
-//                                String image = mRestaurants.get(a).getImageUrl();
-//                                images[a] = image;
-//                            }
-//
-//                            ArrayAdapter<String> adapter = new MyRestaurantsArrayAdapter(RestaurantListActivity.this,
-//                                    android.R.layout.simple_list_item_1, restaurantNames, categories, images );
-//                            mRestaurantsListView.setAdapter(adapter);
-//
-//                            mRestaurantsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//                                @Override
-//                                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                                    Toast.makeText(RestaurantListActivity.this, restaurantNames[position], Toast.LENGTH_SHORT).show();
-//                                }
-//                            });
-//
-//                            for (Restaurant restaurant : mRestaurants) {
-//                                Log.d(TAG, "Name: " + restaurant.getName());
-//                                Log.d(TAG, "Phone: " + restaurant.getPhone());
-//                                Log.d(TAG, "Website: " + restaurant.getWebsite());
-//                                Log.d(TAG, "Image url: " + restaurant.getImageUrl());
-//                                Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
-//                                Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
-//                                Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
-//                            }
-//
+//                        for (int i = 0; i < restaurantNames.length; i++) {
+//                            restaurantNames[i] = mRestaurants.get(i).getName();
 //                        }
-//                    });
-//                }
-//            });
-//        }
+//
+//                        for (int z = 0; z < categories.length; z += 1){
+//                            String category = mRestaurants.get(z).getCategories().get(0);
+//                            categories[z] = category;
+//                        }
+//
+//                        for (int a = 0; a < images.length; a += 1 ){
+//                            String image = mRestaurants.get(a).getImageUrl();
+//                            images[a] = image;
+//                        }
+//
+//                        ArrayAdapter<String> adapter = new MyRestaurantsArrayAdapter(RestaurantListActivity.this,
+//                                android.R.layout.simple_list_item_1, restaurantNames, categories, images );
+//                        mRestaurantsListView.setAdapter(adapter);
+//
+//                        for (Restaurant restaurant : mRestaurants) {
+//                            Log.d(TAG, "Name: " + restaurant.getName());
+//                            Log.d(TAG, "Phone: " + restaurant.getPhone());
+//                            Log.d(TAG, "Website: " + restaurant.getWebsite());
+//                            Log.d(TAG, "Image url: " + restaurant.getImageUrl());
+//                            Log.d(TAG, "Rating: " + Double.toString(restaurant.getRating()));
+//                            Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", restaurant.getAddress()));
+//                            Log.d(TAG, "Categories: " + restaurant.getCategories().toString());
+//                        }
+//
+//                    }
+//                });
+//            }
+//        });
+//    }
 }
