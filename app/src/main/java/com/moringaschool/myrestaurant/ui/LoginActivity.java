f87cef5c693e0a39e9b60d01c1c8a46a8c74bed0
Dhosio <danielgithiomi@gmail.com>
2020-06-01 23:35:32 +0300

    Added the onclick listener to the login activity to redirect to sign up

--- app/src/main/java/com/moringaschool/myrestaurant/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurant/ui/LoginActivity.java
@@ -1,14 +1,42 @@
 package com.moringaschool.myrestaurant.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.moringaschool.myrestaurant.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+//    TAG
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+//    Butter knife binding views
+    @BindView(R.id.registerTextView) TextView mOptionToRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        mOptionToRegisterTextView.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view){
+        if (view == mOptionToRegisterTextView){
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
