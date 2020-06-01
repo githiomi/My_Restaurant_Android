7119a0de878d1bed516aa43bfd3c885022965e0c
Dhosio <danielgithiomi@gmail.com>
2020-06-01 23:13:35 +0300

    Added the login activity and changed default starting activity

---
+++ app/src/main/java/com/moringaschool/myrestaurant/ui/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.moringaschool.myrestaurant.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.moringaschool.myrestaurant.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
