d93f6c27743ab89cf5005e35867917f2a2b5129b
Dhosio <danielgithiomi@gmail.com>
2020-06-08 21:58:32 +0300

    Created the util package and added the interfaces and the item callback class

---
+++ app/src/main/java/com/moringaschool/myrestaurant/util/ItemTouchHelperAdapter.java
@@ -0,0 +1,6 @@
+package com.moringaschool.myrestaurant.util;
+
+public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
+}
---
+++ app/src/main/java/com/moringaschool/myrestaurant/util/OnStartDragListener.java
@@ -0,0 +1,7 @@
+package com.moringaschool.myrestaurant.util;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
